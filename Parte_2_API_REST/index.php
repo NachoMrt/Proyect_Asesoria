cedf10cd0e5cecb2223d96cbd1a49e059e316dea
Nacho <josigmcs@gmail.com>
2026-02-26 14:43:59 +0100

    added factura and servicio models and controllers

--- Parte_2_API_REST/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_GET['api']) && $_GET['api'] === 'productos') {
-
-    require_once ROOT_PATH . "/app/Controllers/ApiProductoController.php";
-
-    $api = new ApiProductoController();
-    $api->handleRequest();
-    exit;
-}
---
+++ Parte_2_API_REST/public/index.php
@@ -0,0 +1,14 @@
+<?php
+
+header('Content-Type: application/json');
+require_once __DIR__ . '/../app/config/Router.php';
+
+$method = $_SERVER['REQUEST_METHOD'];   
+$uri = $_SERVER['REQUEST_URI'];   
+$uri = preg_replace('#^/Certificado/Proyect_Asesoria/Parte_2_API_REST/public(/index\.php)?#', '', $uri);
+$uri = trim($uri, '/'); 
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+$router = new Router();  
+$router->route($method, $uri, $data);
