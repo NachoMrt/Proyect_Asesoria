aa42543a7e0d0531ae6839bc01d79c494bfb60a3
ElenaBuec <Busla2008@hotmail.com>
2026-02-26 14:06:12 +0100

    correcciones

--- Parte 1. MySQL + Model/index.php
+++ Parte 1. MySQL + Model/index.php
@@ -1,3 +1,12 @@
+<?php
+if (isset($_GET['api']) && $_GET['api'] === 'productos') {
+
+    require_once ROOT_PATH . "/app/Controllers/ApiProductoController.php";
+
+    $api = new ApiProductoController();
+    $api->handleRequest();
+    exit;
+}?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
