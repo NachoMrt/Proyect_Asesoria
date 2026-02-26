30a09d8d9276be420d40b9fc8cea3e482ed14643
NachoMrt <josigmcs@gmail.com>
2026-02-26 10:05:33 +0100

    cliente

--- Parte 1. MySQL + Model/model/Cliente.php
+++ Parte 1. MySQL + Model/model/Cliente.php
@@ -1,19 +1,39 @@
+
 <?php
+
+require_once 'config/database.php';
+
 class Cliente
 {
-    public $id_cliente;
-    public $nombre;
-    public $dnie;
-    public $email;
-    public $telefono;
+    private $db;
 
-    public function __construct($nombre, $dnie, $email, $telefono, $id_cliente = null)
+    public function __construct()
     {
-        $this->id_cliente = $id_cliente;
-        $this->nombre = $nombre;
-        $this->dnie = $dnie;
-        $this->email = $email;
-        $this->telefono = $telefono;
+        $this->db = Database::connect();
+    }
+    public function getAll()
+    {
+        return $this->db->query("SELECT * FROM clientes")->fetchAll();
+    }
+    public function getById($id)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM clientes WHERE id_cliente=?");
+        $stmt->execute([$id]);
+        return $stmt->fetch();
+    }
+    public function save($nombre, $email, $dnie, $telefono)
+    {
+        $stmt = $this->db->prepare("INSERT INTO clientes VALUES(NULL,?,?,?,?)");
+        $stmt->execute([$nombre, $dnie, $email, $telefono]);
+    }
+    public function update($id, $nombre, $email, $dnie, $telefono)
+    {
+        $stmt = $this->db->prepare("UPDATE clientes SET nombre=?,email=?, dnie=?, telefono=? WHERE id_cliente=?");
+        $stmt->execute([$nombre, $dnie, $email, $telefono, $id]);
+    }
+    public function delete($id)
+    {
+        $stmt = $this->db->prepare("DELETE FROM clientes WHERE id_cliente=?");
+        $stmt->execute([$id]);
     }
 }
-
