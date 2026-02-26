16d833d7ddf4e25878a139f2d1e5d131683db0aa
Nacho <joseigncs@gmail.com>
2026-02-26 10:01:29 +0100

    Asesor Factura y Servicio

--- Parte 1. MySQL + Model/model/Servicio.php
+++ Parte 1. MySQL + Model/model/Servicio.php
@@ -1,14 +1,42 @@
 <?php
+require_once 'config/database.php';
+
 class Servicio
 {
-    public $id_servicio;
-    public $nombre_servicio;
-    public $precio;
+    private $db;
 
-    public function __construct($nombre_servicio, $precio, $id_servicio = null)
+    public function __construct()
     {
-        $this->id_servicio = $id_servicio;
-        $this->nombre_servicio = $nombre_servicio;
-        $this->precio = $precio;
+        $this->db = Database::connect();
+    }
+
+    public function getAll(): array
+    {
+        return $this->db->query("SELECT * FROM servicios")->fetchAll();
+    }
+
+    public function getById($id): mixed
+    {
+        $stmt = $this->db->prepare("SELECT * FROM servicios WHERE id_servicio=?");
+        $stmt->execute([$id]);
+        return $stmt->fetch();
+    }
+
+    public function save($nombre, $precio): void
+    {
+        $stmt = $this->db->prepare("INSERT INTO servicios (nombre_servicio, precio) VALUES (?,?)");
+        $stmt->execute([$nombre, $precio]);
+    }
+
+    public function update($id, $nombre, $precio): void
+    {
+        $stmt = $this->db->prepare("UPDATE servicios SET nombre_servicio=?, precio=? WHERE id_servicio=?");
+        $stmt->execute([$nombre, $precio, $id]);
+    }
+
+    public function delete($id): void
+    {
+        $stmt = $this->db->prepare("DELETE FROM servicios WHERE id_servicio=?");
+        $stmt->execute([$id]);
     }
 }
